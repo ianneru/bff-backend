381f586d3833d91c3bf5502eda63e74662ea8b16
Ian Letelier <ian_neru@hotmail.com>
2021-09-29 13:36:45 -0300

    Initial commit

---
+++ Host/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Host.Controller
+{
+    [Route("user")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("info")]
+        public IActionResult GetUser()
+        {
+            _logger.LogInformation($"Getting user information for {User?.Identity?.Name}");
+            var user = new { name = User.Identity.Name };
+
+            return new JsonResult(user);
+        }
+
+        [Route("logout")]
+        public IActionResult Logout()
+        {
+            _logger.LogInformation($"Logging out user {User?.Identity?.Name}");
+            return SignOut("cookies", "oidc");
+        }
+    }
+}
